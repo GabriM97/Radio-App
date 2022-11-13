29a60e5bc029ed2f21c7c2b71c3064576945d717
Gabriele <marino.gabri97@gmail.com>
2022-11-13 11:14:28 +0000

    add annotations and use them for webhook controller

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class WebhookController
 {
+    #[Route('/webhook', 'webhook')]
     public function index(): Response
     {
         return new Response('Hello World');
