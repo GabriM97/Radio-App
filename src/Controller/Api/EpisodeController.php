d9e69b0fbe6e1bac7d2c3fd1a1b345676d380909
Gabriele <marino.gabri97@gmail.com>
2022-11-14 00:44:56 +0000

    add routes to show and store episodes

--- src/Controller/Api/EpisodeController.php
+++ src/Controller/Api/EpisodeController.php
@@ -3,25 +3,70 @@
 namespace App\Controller\Api;
 
 use App\Controller\AbstractController;
+use App\Entity\Episode;
+use App\Entity\User;
+use App\Repository\EpisodeRepository;
+use App\Repository\PodcastRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api', name: 'api_')]
 class EpisodeController extends AbstractController
 {
-    #[Route('/episode/{id}/stats', name: 'episode_stats', methods: ['GET', 'HEAD'], requirements: ['page' => '\d+'])]
-    public function getStats(int $id): JsonResponse
+    #[Route('/episode/create', name: 'episode_create', methods: ['POST'])]
+    public function store(
+        Request $request, 
+        EpisodeRepository $episodeRepository, 
+        PodcastRepository $podcastRepository,
+        UserRepository $userRepository
+    ): JsonResponse {
+        $data = $this->getRequestData($request);
+        
+        $episode = new Episode();
+        $episode->setTitle($data['title']);
+        $episode->setTopic($data['topic']);
+        $episode->setPodcast($podcastRepository->find($data['podcast_id']));
+        foreach($data['hosts'] as $host) {
+            $episode->addHost($userRepository->find($host['id']));
+        }
+
+        $episodeRepository->save($episode, true);
+
+        return $this->json(['id' => $episode->getId()]);
+    }
+
+    #[Route('/episode/{id}', name: 'episode', methods: ['GET', 'HEAD'])]
+    public function show(int $id, EpisodeRepository $episodeRepository): JsonResponse
     {
+        $episode = $episodeRepository->find($id);
+
         return $this->json([
-            'stats' => [
-                'id' => $id,
-                'total_views' => 1000,
-                'total_downloads' => 200,
-                '01/01/2022' => [
-                    'views' => 100,
-                    'downloads' => 25,
-                ],
-            ]
+            'id' => $episode->getId(),
+            'title' => $episode->getTitle(),
+            'topic' => $episode->getTopic(),
+            'podcast_id' => $episode->getPodcast()->getId(),
+            'hosts' => array_map(
+                fn(User $host) => ['id' => $host->getId()], 
+                $episode->getHosts()->toArray()
+            ),
         ]);
     }
+
+    // #[Route('/episode/{id}/stats', name: 'episode_stats', methods: ['GET', 'HEAD'], requirements: ['page' => '\d+'])]
+    // public function getStats(int $id): JsonResponse
+    // {
+    //     return $this->json([
+    //         'stats' => [
+    //             'id' => $id,
+    //             'total_views' => 1000,
+    //             'total_downloads' => 200,
+    //             '01/01/2022' => [
+    //                 'views' => 100,
+    //                 'downloads' => 25,
+    //             ],
+    //         ]
+    //     ]);
+    // }
 }
